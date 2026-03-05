5b4155db1a88f0452d5448c07987557242836ba6
woaiwan2010 <49775693+woaiwan2010@users.noreply.github.com>
2026-03-05 14:54:34 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   
   return {
-    base: '/-/', // ✨ 正确配置的 GitHub 仓库名
+    base: '/realtimeui/', // ✨ 正确配置的 GitHub 仓库名
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
